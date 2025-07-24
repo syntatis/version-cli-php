3cba3befde8c16766e1b8f930d280db35e48b809
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-07-24 22:58:21 +0700

    Initial commits

---
+++ app/Increment.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Syntatis\Version\CLI;
+
+final class Increment
+{
+}
---
+++ tests/app/IncrementTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Syntatis\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class IncrementTest extends TestCase
+{
+	public function testTrue(): void
+	{
+		$this->assertTrue(true);
+	}
+}
