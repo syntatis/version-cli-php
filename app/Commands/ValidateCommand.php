f2cae214c06a42b38ad0d70fe34a4129e01a6e3e
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-07-28 10:33:18 +0700

    Fix import argument namespace

--- app/Commands/ValidateCommand.php
+++ app/Commands/ValidateCommand.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Syntatis\Version\CLI\Concerns\InvalidArgumentType;
+use Syntatis\Version\CLI\Exceptions\InvalidArgumentType;
 use Throwable;
 use Version\Version;
 
